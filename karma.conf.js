9f35a46ea43f21de671fe8041435c2ba779341b7
Rui Leite <ruipimentelleite@gmail.com>
2026-06-19 20:26:49 -0300

    Configura o ambiente para testes unitários no Firefox.
    - Ideal para instalações Linux limpas.

--- karma.conf.js
+++ karma.conf.js
@@ -8,6 +8,7 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -37,18 +38,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: [
-      'ChromeHeadlessNoSandbox',
-    ],
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: "ChromeHeadless",
-        flags: [
-          "--no-sandbox",
-          "--disable-setuid-sandbox",
-        ],
-      },
-    },
+    browsers: ['FirefoxHeadless'],
     singleRun: false,
     restartOnFileChange: true
   });
